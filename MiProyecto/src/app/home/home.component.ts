4c3e7ac96a720a6b8faafbb478593fc598be3758
ElvisZM <elfabri28@gmail.com>
2024-02-12 13:45:54 +0000

    Continuar con Watchlist

--- MiProyecto/src/app/home/home.component.ts
+++ MiProyecto/src/app/home/home.component.ts
@@ -24,18 +24,14 @@ export class HomeComponent implements OnInit {
   getPopularMovies() {
     this.http.get<any>('https://api.themoviedb.org/3/movie/popular?api_key=665eddc29536d1ffc4e5fdace47ae8c7')
       .subscribe(response => {
-        console.log(response); // Verifica la respuesta de la API
         this.popularMovies = response.results.slice(0, 3);
-        console.log(this.popularMovies); // Verifica los datos de las películas populares
       });
   }
 
   getTopRatedMovies() {
     this.http.get<any>('https://api.themoviedb.org/3/movie/top_rated?api_key=665eddc29536d1ffc4e5fdace47ae8c7')
       .subscribe(response => {
-        console.log(response); // Verifica la respuesta de la API
-        this.topRatedMovies = response.results;
-        console.log(this.topRatedMovies); // Verifica los datos de las películas mejor valoradas
+        this.topRatedMovies = response.results.slice(0, 15);
       });
   }
 
--- MiProyecto/src/app/watchlist/watchlist.component.ts
+++ MiProyecto/src/app/watchlist/watchlist.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-watchlist',
   templateUrl: './watchlist.component.html',
   styleUrl: './watchlist.component.scss'
 })
-export class WatchlistComponent {
+export class WatchlistComponent implements OnInit{
+  popularMovies: any[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+
+  this.getPopularMovies();
+}
+
+getPopularMovies() {
+  this.http.get<any>('https://api.themoviedb.org/3/movie/popular?api_key=665eddc29536d1ffc4e5fdace47ae8c7')
+    .subscribe(response => {
+      this.popularMovies = response.results.slice(0, 15);
+    });
+ }
 }
